057031e0adbce8b293901304f179e6b8e8ec2e87
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2017-02-21 00:16:16 +0100

    scripts: add initial helper facts for prepare domain
    The prepare domain must be primed with facts that the next CS prepare
    operation is for retrieving, from then on it'll be handled
    automatically.
    
    Additionally, call planner in script.

--- scripts/rcll_prepare_1robot.sh
+++ scripts/rcll_prepare_1robot.sh
@@ -4,10 +4,11 @@ source $(dirname $(realpath $0))/rosplan_functions.sh
 
 #INSTANCES="C-BS:mps C-DS:mps C-CS1:mps C-CS2:mps C-RS1:mps C-RS2:mps"
 #FACTS="mps-type;m:C-BS|t:BS mps-type;m:C-DS|t:DS mps-type;m:C-CS1|t:CS mps-type;m:C-CS2|t:CS mps-type;m:C-RS1|t:RS mps-type;m:C-RS2|t:RS"
+FACTS="cs-can-perform;m:C-CS1|op:CS_RETRIEVE cs-can-perform;m:C-CS2|op:CS_RETRIEVE"
 GOALS="prepared;m:C-BS prepared;m:C-DS prepared;m:C-CS1 prepared;m:C-CS2 prepared;m:C-RS1 prepared;m:C-RS2"
 
 rosplan_clear_all
 rosplan_add_instances $INSTANCES
 rosplan_add_facts $FACTS
 rosplan_add_goals $GOALS
-#  rosplan_call_planner
+rosplan_call_planner
